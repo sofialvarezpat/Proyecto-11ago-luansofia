2dac895a84bfe053a31747d83465e256e620200c
SofiaA <salvarezp@ucundinamarca.edu.co>
2026-08-12 17:48:46 -0500

    getter and setters de la case vehiculo

--- src/main/java/org/example/Vehiculo.java
+++ src/main/java/org/example/Vehiculo.java
@@ -11,7 +11,52 @@ public abstract class Vehiculo {
     public Vehiculo() {
     }
 
-    public Vehiculo(String ID, String nombre, Double salarioBase) { //esto es para editar
+    public Vehiculo(String Placa, String Marca, String Modelo, int Anio, double preciobase) {//esto es para editar
+        this.placa = Placa;
+        this.marca = Marca;
+        this.modelo = Modelo;
+        this.anio = Anio;
+        this.preciobase = preciobase;
 
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public double getPreciobase() {
+        return preciobase;
+    }
+
+    public void setPreciobase(double preciobase) {
+        this.preciobase = preciobase;
     }
 }
