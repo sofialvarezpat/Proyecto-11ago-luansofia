e97d31c12c16acef60653beff080e647f3020bed
SofiaA <salvarezp@ucundinamarca.edu.co>
2026-08-12 17:36:15 -0500

    superclase en edicion + eliminación de las subclases

--- src/main/java/org/example/Camión_Carga.java
+++
@@ -1,4 +0,0 @@
-package org.example;
-
-public class Camión_Carga extends Vehiculo{
-}
--- src/main/java/org/example/Vehiculo.java
+++ src/main/java/org/example/Vehiculo.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public abstract class Vehiculo {
+    protected String placa;
+    protected String marca;
+    protected String modelo;
+    protected int anio;
+    protected double preciobase;
+
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String ID, String nombre, Double salarioBase) { //esto es para editar
+
+    }
 }
